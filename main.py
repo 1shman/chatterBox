1f032d4f61f0abe26820d4bd5b23ea79931afec9
1shman <ishman@umich.edu>
2024-05-08 19:33:33 -0400

    Rearrange file setup, main.py show_index

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import flask
 import flask_socketio
 import string
 
-#TODO: review this
+#TODO: review boilerplate
 app = flask.Flask(__name__)
 app.config["SECRET_KEY"] = "filler"
 socketio = flask_socketio.SocketIO(app)
+
+@app.route("/", methods=["GET"])
+def show_index(): 
+    return flask.render_template("index.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
