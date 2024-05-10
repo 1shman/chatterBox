4ee43cfca30c4e3063589165b2eaebb2877f196d
1shman <ishman@umich.edu>
2024-05-09 22:04:43 -0400

    socketio connect/disconnect and debugging

--- main.py
+++ main.py
@@ -18,37 +18,36 @@ def codeGenerator(length):
             break
     return code
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def show_index(): 
     flask.session.clear()
-    return flask.render_template("home.html")
+    if flask.request.method == "POST":
+        name = flask.request.form.get("name")
+        roomCode = flask.request.form.get("roomCode")
+        join = flask.request.form.get("join", False)
+        create = flask.request.form.get("create", False)
 
-@app.route("/", methods=["POST"])
-def post_index(): 
-    name = flask.request.form.get("name")
-    roomCode = flask.request.form.get("roomCode")
-    join = flask.request.form.get("join", False)
-    create = flask.request.form.get("create", False)
+        #error checking
+        if not name: 
+            return flask.render_template("home.html", error="Please enter your name.", roomCode=roomCode, name=name)
+        #TODO: revist join != false
+        if join != False and not roomCode: 
+            return flask.render_template("home.html", error="Please enter a room code.", roomCode=roomCode, name=name)
+        
+        room = roomCode
+        if create != False:
+            room = codeGenerator(4)
+            rooms[room] = {"members": 0, "messages": []}
 
-    #error checking
-    if not name: 
-        return flask.render_template("home.html", error="Please enter your name.", roomCode=roomCode, name=name)
-    #TODO: revist join != false
-    if join != False and not roomCode: 
-        return flask.render_template("home.html", error="Please enter a room code.", roomCode=roomCode, name=name)
+        elif roomCode not in rooms:
+            return flask.render_template("home.html", error="Room Does not exist.", roomCode=roomCode, name=name)
+
+        flask.session["room"] = room
+        flask.session["name"] = name
+
+        return flask.redirect(flask.url_for("show_room"))
     
-    room = roomCode
-    if create != False:
-        room = codeGenerator(4)
-        rooms[room] = {"members": 0, "messages": []}
-
-    elif roomCode not in rooms:
-        return flask.render_template("home.html", error="Room Does not exist.", roomCode=roomCode, name=name)
-
-    flask.session["room"] = room
-    flask.session["name"] = name
-
-    return flask.redirect(flask.url_for("show_room"))
+    return flask.render_template("home.html")
 
 @app.route("/room")
 def show_room():
@@ -56,7 +55,7 @@ def show_room():
     room = flask.session.get("room")
     name = flask.session.get("name")
     if room is None or name is None or room not in rooms:
-        return flask.redirect(flask.url_for("home"))
+        return flask.redirect(flask.url_for("show_index")) #bug issues/resolved
 
     return flask.render_template("room.html")
 
@@ -74,6 +73,18 @@ def connect(auth):
     flask_socketio.send({"name": name, "message": "has entered the room"}, to="room")
     rooms[room]["members"] += 1
     print(f"{name} joined room {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = flask.session.get("room")
+    name = flask.session.get("name")
+
+    if room in rooms: 
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] < 1:
+            del rooms[room]
+    flask_socketio.send({"name": name, "message": "has left the room"}, to="room")
+    print(f"{name} left room {room}")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
